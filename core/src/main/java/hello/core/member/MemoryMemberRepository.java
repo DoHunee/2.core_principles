5f4c0a965b21d2230732e0f4e159762cde7479f5
장도훈 <jehgns1@gmail.com>
2024-07-12 15:48:48 +0900

    회원 도메인 실행과 테스트

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,25 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+   // Member 객체를 저장하는 Map, 회원 ID를 키로 사용한다.
+   // 동시성 이슈가 발생할 수 있지만 개발용도로 단순히 테스트 용도로 사용한다.
+  private static Map<Long, Member> store = new HashMap<>();
+
+  @Override
+  public void save(Member member) {
+    // 회원 객체를 store 맵에 저장
+    store.put(member.getId(), member);
+    
+  }
+  @Override
+  public Member findById(Long memberid) {
+    // 회원 객체를 store 맵에 저장
+    return store.get(memberid);
+  }
+  
+
+}
---
+++ core/src/test/java/hello/core/MemberServiceTest/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.MemberServiceTest;
+
+import org.junit.jupiter.api.Test;
+
+import hello.core.member.Grade;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+
+public class MemberServiceTest {
+
+  // MemberService 구현체 생성
+  MemberService memberService = new MemberServiceImpl();
+
+  @Test
+  void join() {
+    
+    // given - 테스트를 위한 준비 단계
+    Member member = new Member(1L, "memberA", Grade.VIP); // when
+    
+    // when - 실제 테스트 동작 수행
+    memberService.join(member);
+    Member findMember = memberService.findMember(1L);
+    
+    // then - 테스트 결과 검증
+    Assertions.assertThat(member).isEqualTo(findMember);
+  }
+
+}
