04189af9b5f971418adc8a66b8824276fb9fdcd7
장도훈 <jehgns1@gmail.com>
2024-07-17 15:55:24 +0900

    결론적으로 공유 필드를 없애고 문제점 제거해서 정상적으로 돌아가게 수정!

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 
 public class StatefulServiceTest {
   @Test
@@ -19,24 +19,25 @@ public class StatefulServiceTest {
     
     
     // ThreadA: A사용자 10000원 주문
-    statefulService1.order("userA", 10000);
+    int userAPrice = statefulService1.order("userA", 10000);
     
     // ThreadB: B사용자 20000원 주문
-    statefulService2.order("userB", 20000);
+    int userBPrice = statefulService2.order("userB", 20000);
     
     // ThreadA: 사용자A 주문 금액 조회
-    int price = statefulService1.getPrice();
+    // int price = statefulService1.getPrice();
     
     // ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
     // StatefulService의 price 공유필드에 의해 20000원이 출력된다.
     // 진짜 공유 필드는 조심해야해!
-    System.out.println("price = " + price);
+    System.out.println("userAprice = " + userAPrice);
+    System.out.println("userBPrice = " + userBPrice);
     
     
-    Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    // Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
   }
 
-    // @Configuration
+    @Configuration
     static class TestConfig {
       @Bean
       public StatefulService statefulService() {
