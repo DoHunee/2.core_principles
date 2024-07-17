f7429364448a7001318409969ac8836ba2defeb9
장도훈 <jehgns1@gmail.com>
2024-07-17 17:28:30 +0900

    Configuration 있고 없고의 빈등록 차이점 Test 완료!

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -10,24 +10,37 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import static org.assertj.core.api.Assertions.*;
 
 public class ConfigurationSingletonTest {
-  
+
   @Test
   void configurationTest() {
     @SuppressWarnings("resource")
     ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-    
+
     // 3개다 값이 같다!!!
-    MemberServiceImpl memberService = ac.getBean("memberService",MemberServiceImpl.class);
+    MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
     OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
-    MemberRepository memberRepository = ac.getBean("memberRepository",MemberRepository.class);
-    
+    MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
     // 모두 같은 인스턴스를 참고하고 있다.
     System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
     System.out.println("orderService -> memberRepository = " + orderService.getMemberRepository());
     System.out.println("memberRepository = " + memberRepository);
-    
+
     // 모두 같은 인스턴스를 참고하고 있다.
     assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
     assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
   }
+
+  @Test
+  void configurationDeep() {
+    @SuppressWarnings("resource")
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    // AppConfig도 스프링 빈으로 등록된다.
+    AppConfig bean = ac.getBean(AppConfig.class);
+
+    System.out.println("bean = " + bean.getClass());
+    // 출력: bean = class hello.core.AppConfig$$SpringCGLIB$$0
+    // 순수한 클래스라면 class hello.core.AppConfig가 나와야 함!
+  }
+
 }
