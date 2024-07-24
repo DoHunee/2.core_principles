9ca4ef12c54c9fb7c4a2cfb43a4ac61ffc83da9b
장도훈 <jehgns1@gmail.com>
2024-07-24 18:05:46 +0900

    프로토타입 스코프 싱글톤 빈과 함께 사용시 싱글톤 빈이 프로토타입을 사용할때마다 컨테이너에 요청 하는 코드!

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -19,7 +20,6 @@ public class SingletonWithPrototypeTest1 {
     @SuppressWarnings("resource")
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
 
-
     // PrototypeBean을 컨텍스트에서 두 번 가져와서 각각 count를 증가시키고 확인
     PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
     prototypeBean1.addCount();
@@ -34,37 +34,65 @@ public class SingletonWithPrototypeTest1 {
   @Test
   void singletonClientUsePrototype() {
     @SuppressWarnings("resource")
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
-    
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,
+        PrototypeBean.class);
+
     // ClientBean을 컨텍스트에서 두 번 가져와서 각각 logic 메소드를 호출하고 결과를 확인
     ClientBean clientBean1 = ac.getBean(ClientBean.class);
     int count1 = clientBean1.logic();
     assertThat(count1).isEqualTo(1);
-    
+
     ClientBean clientBean2 = ac.getBean(ClientBean.class);
     int count2 = clientBean2.logic();
-    assertThat(count2).isEqualTo(2);  // 여기서 2가 나오는 이유는 ClientBean이 싱글톤이기 때문에 prototypeBean이 재사용되기 때문임
+    assertThat(count2).isEqualTo(2); // 여기서 2가 나오는 이유는 ClientBean이 싱글톤이기 때문에 prototypeBean이 재사용되기 때문임
   }
 
+  @Test
+  void providerTest() {
+    @SuppressWarnings("resource")
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,
+        PrototypeBean.class);
 
-  // ClientBean 클래스 정의 - 싱글톤 스코프
-  @Scope("singleton")
+    ClientBean clientBean1 = ac.getBean(ClientBean.class);
+    int count1 = clientBean1.logic();
+    assertThat(count1).isEqualTo(1);
+
+    ClientBean clientBean2 = ac.getBean(ClientBean.class);
+    int count2 = clientBean2.logic();
+    assertThat(count2).isEqualTo(1);
+  }
+
   static class ClientBean {
-    private final PrototypeBean prototypeBean; //생성시점에 주입
-
-    // ClientBean 생성자 - 프로토타입 빈을 주입 받음
     @Autowired
-    public ClientBean(PrototypeBean prototypeBean) {
-      this.prototypeBean = prototypeBean;
-    }
+    private ApplicationContext ac;
 
-    // 비즈니스 로직 메소드 - 프로토타입 빈의 count를 증가시키고 반환
     public int logic() {
+      PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
       prototypeBean.addCount();
       int count = prototypeBean.getCount();
       return count;
     }
   }
+
+  // ClientBean 클래스 정의 - 싱글톤 스코프
+  // @Scope("singleton")
+  // static class ClientBean {
+
+  //   private final PrototypeBean prototypeBean;
+
+  //   // ClientBean 생성자 - 프로토타입 빈을 주입 받음
+  //   @Autowired
+  //   public ClientBean(PrototypeBean prototypeBean) {
+  //     this.prototypeBean = prototypeBean;
+  //   }
+
+  //   // 비즈니스 로직 메소드 - 프로토타입 빈의 count를 증가시키고 반환
+  //   public int logic() {
+  //     prototypeBean.addCount();
+  //     int count = prototypeBean.getCount();
+  //     return count;
+  //   }
+  // }
 
   // PrototypeBean 클래스 정의 - 프로토타입 스코프
   @Scope("prototype")
@@ -87,7 +115,7 @@ public class SingletonWithPrototypeTest1 {
       System.out.println("PrototypeBean.init " + this);
     }
 
-     // 빈 소멸 메소드
+    // 빈 소멸 메소드
     @PreDestroy
     public void destroy() {
       System.out.println("PrototypeBean.destroy");
